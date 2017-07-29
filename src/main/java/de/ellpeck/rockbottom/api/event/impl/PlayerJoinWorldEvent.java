7cde682572e16119cd8e4428af517afab357def4
Ellpeck <megamaximal@gmail.com>
2017-07-29 15:31:36 +0200

    added player join and leave world events

---
+++ src/main/java/de/ellpeck/rockbottom/api/event/impl/PlayerJoinWorldEvent.java
@@ -0,0 +1,37 @@
+/*
+ * This file ("PlayerJoinWorldEvent.java") is part of the RockBottomAPI by Ellpeck.
+ * View the source code at <https://github.com/Ellpeck/RockBottomAPI>.
+ *
+ * The RockBottomAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The RockBottomAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the RockBottomAPI. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.ellpeck.rockbottom.api.event.impl;
+
+import de.ellpeck.rockbottom.api.entity.player.AbstractEntityPlayer;
+import de.ellpeck.rockbottom.api.event.Event;
+
+/**
+ * This event is fired when an {@link AbstractEntityPlayer} joins an {@link de.ellpeck.rockbottom.api.world.IWorld}
+ * <br> It is not cancellable
+ */
+public class PlayerJoinWorldEvent extends Event{
+
+    public final AbstractEntityPlayer player;
+    public final boolean isConnected;
+
+    public PlayerJoinWorldEvent(AbstractEntityPlayer player, boolean isConnected){
+        this.player = player;
+        this.isConnected = isConnected;
+    }
+}
