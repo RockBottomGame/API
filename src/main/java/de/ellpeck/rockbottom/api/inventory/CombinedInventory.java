ddea9bca7c3ad7d418a038f1d8f67de4f9640784
Ellpeck <megamaximal@gmail.com>
2017-08-03 15:55:30 +0200

    a lot of changey stuff and also CombinedInventory formatting and fixes

--- src/main/java/de/ellpeck/rockbottom/api/inventory/CombinedInventory.java
+++ src/main/java/de/ellpeck/rockbottom/api/inventory/CombinedInventory.java
@@ -4,59 +4,55 @@ import de.ellpeck.rockbottom.api.item.ItemInstance;
 
 import java.util.function.BiFunction;
 
-public class CombinedInventory implements IInventory {
+public class CombinedInventory implements IInventory{
+
     private final IInventory[] inventories;
 
-    public CombinedInventory(IInventory... inventories) {
+    public CombinedInventory(IInventory... inventories){
         this.inventories = inventories;
     }
 
     @Override
-    public void set(int id, ItemInstance instance) {
-        this.<Void>executeOnSlot(id, (inventory, integer) -> {
+    public void set(int id, ItemInstance instance){
+        this.executeOnInv(id, (inventory, integer) -> {
             inventory.set(integer, instance);
             return null;
         });
     }
 
-    public <T> T executeOnSlot(int id, BiFunction<IInventory, Integer, T> function) {
-        for (int i = 0; i < inventories.length; i++) {
-            int offset = id - offset(i);
-            if (inventories[i].getSlotAmount() > offset) {
-                return function.apply(inventories[i], offset);
+    public <T> T executeOnInv(int id, BiFunction<IInventory, Integer, T> function){
+        int slotCounter = 0;
+        for(IInventory inventory : this.inventories){
+            int amount = inventory.getSlotAmount();
+            if(slotCounter+amount > id){
+                return function.apply(inventory, id-slotCounter);
+            }
+            else{
+                slotCounter += amount;
             }
         }
-        throw new IndexOutOfBoundsException("Id: " + id + " is not within the bounds of the contained inventories");
+        return null;
     }
 
-    private int offset(int i) {
+    @Override
+    public ItemInstance add(int id, int amount){
+        return this.executeOnInv(id, (inventory, integer) -> inventory.add(integer, amount));
+    }
+
+    @Override
+    public ItemInstance remove(int id, int amount){
+        return this.executeOnInv(id, (inventory, integer) -> inventory.remove(integer, amount));
+    }
+
+    @Override
+    public ItemInstance get(int id){
+        return this.executeOnInv(id, IInventory:: get);
+    }
+
+    @Override
+    public int getSlotAmount(){
         int sum = 0;
-        for (int j = 0; j < i; j++) {
-            int slotAmount = inventories[j].getSlotAmount();
-            sum += slotAmount;
-        }
-        return sum;
-    }
-
-    @Override
-    public ItemInstance add(int id, int amount) {
-        return this.executeOnSlot(id, (inventory, integer) -> inventory.add(integer, amount));
-    }
-
-    @Override
-    public ItemInstance remove(int id, int amount) {
-        return this.executeOnSlot(id, (inventory, integer) -> inventory.remove(integer, amount));
-    }
-
-    @Override
-    public ItemInstance get(int id) {
-        return this.executeOnSlot(id, IInventory::get);
-    }
-
-    @Override
-    public int getSlotAmount() {
-        int sum = 0;
-        for (IInventory inventory : inventories) {
+        for(IInventory inventory : this.inventories){
             int slotAmount = inventory.getSlotAmount();
             sum += slotAmount;
         }
@@ -64,23 +60,23 @@ public class CombinedInventory implements IInventory {
     }
 
     @Override
-    public void notifyChange(int slot) {
-        this.executeOnSlot(slot, (inv, i) -> {
+    public void notifyChange(int slot){
+        this.executeOnInv(slot, (inv, i) -> {
             inv.notifyChange(i);
             return null;
         });
     }
 
     @Override
-    public void addChangeCallback(IInvChangeCallback callback) {
-        for (IInventory inventory : inventories) {
+    public void addChangeCallback(IInvChangeCallback callback){
+        for(IInventory inventory : this.inventories){
             inventory.addChangeCallback(callback);
         }
     }
 
     @Override
-    public void removeChangeCallback(IInvChangeCallback callback) {
-        for (IInventory inventory : inventories) {
+    public void removeChangeCallback(IInvChangeCallback callback){
+        for(IInventory inventory : this.inventories){
             inventory.removeChangeCallback(callback);
         }
     }
