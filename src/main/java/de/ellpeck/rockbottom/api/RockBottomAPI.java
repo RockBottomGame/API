e171de1b080613928781dbbcb9aaf71f83c8ec71
Ellpeck <megamaximal@gmail.com>
2017-08-03 21:32:18 +0200

    bump all the versions

--- src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
+++ src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
@@ -54,7 +54,7 @@ public final class RockBottomAPI{
     /**
      * The current API version
      */
-    public static final String VERSION = "0.0.21";
+    public static final String VERSION = "0.0.22";
 
     /**
      * The registry for {@link Tile}
