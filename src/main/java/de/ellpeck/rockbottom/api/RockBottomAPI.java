7555d094706d23e486fd9c16dd0181547d64a374
Ellpeck <megamaximal@gmail.com>
2017-08-03 16:05:42 +0200

    versiony bumpy

--- src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
+++ src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
@@ -53,7 +53,7 @@ public final class RockBottomAPI{
     /**
      * The current API version
      */
-    public static final String VERSION = "0.0.19";
+    public static final String VERSION = "0.0.20";
 
     /**
      * The registry for {@link Tile}
