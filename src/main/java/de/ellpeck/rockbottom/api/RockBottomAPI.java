d18ef5a12352df4d90ea56f340ed97378cd6ba21
ellpeck <megamaximal@gmail.com>
2017-12-27 22:39:58 +0100

    make localizations use the dot system too

--- src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
+++ src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
@@ -67,7 +67,7 @@ public final class RockBottomAPI{
     /**
      * The current API version equal to the version in the build.gradle file.
      */
-    public static final String VERSION = "0.1.5";
+    public static final String VERSION = "0.1.6";
 
     @ApiInternal
     private static final List<IRegistry> ALL_REGISTRIES = new ArrayList<>();
