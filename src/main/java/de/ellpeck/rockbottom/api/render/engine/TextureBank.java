32e60c9a64aeaea2406e60e508c1f473a70c3be6
Ellpeck <megamaximal@gmail.com>
2018-01-20 13:09:39 +0100

    added the other 16 texture banks just for completion

--- src/main/java/de/ellpeck/rockbottom/api/render/engine/TextureBank.java
+++ src/main/java/de/ellpeck/rockbottom/api/render/engine/TextureBank.java
@@ -21,6 +21,14 @@
 
 package de.ellpeck.rockbottom.api.render.engine;
 
+import org.lwjgl.opengl.GL13;
+
+/**
+ * Texture banks as defined by {@link GL13#GL_TEXTURE0} through {@link
+ * GL13#GL_TEXTURE31}. As these texture banks are not necessarily all supported
+ * by graphics cards (as documented in {@link GL13#glActiveTexture(int)}, please
+ * use these sparingly and try to stay within the first eight or so banks.
+ */
 public enum TextureBank{
     BANK_1,
     BANK_2,
@@ -37,7 +45,23 @@ public enum TextureBank{
     BANK_13,
     BANK_14,
     BANK_15,
-    BANK_16;
+    BANK_16,
+    BANK_17,
+    BANK_18,
+    BANK_19,
+    BANK_20,
+    BANK_21,
+    BANK_22,
+    BANK_23,
+    BANK_24,
+    BANK_25,
+    BANK_26,
+    BANK_27,
+    BANK_28,
+    BANK_29,
+    BANK_30,
+    BANK_31,
+    BANK_32;
 
     public static final TextureBank[] BANKS = values();
 }
