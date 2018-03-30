7b886145261cb0fc557f2cd9c2de21b53e32570b
Ellpeck <megamaximal@gmail.com>
2018-03-30 03:00:10 +0200

    fix toggle buttons not being able to be used by mods despite the fact that they were in the API

--- src/main/java/de/ellpeck/rockbottom/api/gui/component/ComponentToggleButton.java
+++ src/main/java/de/ellpeck/rockbottom/api/gui/component/ComponentToggleButton.java
@@ -24,25 +24,32 @@ package de.ellpeck.rockbottom.api.gui.component;
 import de.ellpeck.rockbottom.api.IGameInstance;
 import de.ellpeck.rockbottom.api.RockBottomAPI;
 import de.ellpeck.rockbottom.api.gui.Gui;
+import de.ellpeck.rockbottom.api.mod.IMod;
 import de.ellpeck.rockbottom.api.util.reg.IResourceName;
 
 import java.util.function.Supplier;
 
 public class ComponentToggleButton extends ComponentButton{
 
+    private final IMod locDomain;
     private final String locKey;
     private boolean isToggled;
 
     public ComponentToggleButton(Gui gui, int x, int y, int sizeX, int sizeY, boolean defaultState, Supplier<Boolean> supplier, String locKey, String... hover){
+        this(gui, x, y, sizeX, sizeY, defaultState, supplier, RockBottomAPI.getGame(), locKey, hover);
+    }
+
+    public ComponentToggleButton(Gui gui, int x, int y, int sizeX, int sizeY, boolean defaultState, Supplier<Boolean> supplier, IMod locDomain, String locKey, String... hover){
         super(gui, x, y, sizeX, sizeY, supplier, null, hover);
         this.isToggled = defaultState;
+        this.locDomain = locDomain;
         this.locKey = locKey;
     }
 
     @Override
     protected String getText(){
         String resource = this.locKey+(this.isToggled ? "_toggled" : "");
-        return RockBottomAPI.getGame().getAssetManager().localize(RockBottomAPI.createInternalRes(resource));
+        return RockBottomAPI.getGame().getAssetManager().localize(RockBottomAPI.createRes(this.locDomain, resource));
     }
 
     @Override
