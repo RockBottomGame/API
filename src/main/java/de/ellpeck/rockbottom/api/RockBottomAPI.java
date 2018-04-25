e3cf1631010cb5bd2e9260514511c95479bea172
Ellpeck <megamaximal@gmail.com>
2018-04-25 13:33:03 +0200

    bump API version

--- src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
+++ src/main/java/de/ellpeck/rockbottom/api/RockBottomAPI.java
@@ -77,7 +77,7 @@ public final class RockBottomAPI{
     /**
      * The current API version equal to the version in the build.gradle file.
      */
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.3.1";
 
     /**
      * A list of all the registries in the game. This is only used for
